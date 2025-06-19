32a26b23649349d43484ce3021d9de39d5771f4e
Thisharu-Dilhara <thisharud@gmail.com>
2025-06-19 12:30:12 +0530

    add method to display hang man

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.security.SignedObject;
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -15,11 +17,55 @@ public class Main {
         for(int i=0; i<word.length(); i++){
             wordState.add('_');
         }
+
+
         System.out.println("************************");
         System.out.println("Welcome to java hangman");
         System.out.println("************************");
 
+        while (wrongGuess < 6){
 
+            System.out.println(getHangmanArt(wrongGuess));
+
+            System.out.print("Word: ");
+
+            for(char c : wordState){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+
+            System.out.print("Guess a letter: ");
+            char guess = scanner.next().toLowerCase().charAt(0);
+
+            if (word.indexOf(guess) >= 0 ){
+                System.out.println("Correct guess");
+
+                for (int i=0; i<word.length(); i++){
+                    if (word.charAt(i) == guess){
+                        wordState.set(i,guess);
+                    }
+                }
+
+                if (!wordState.contains('_')){
+                    System.out.println(getHangmanArt(wrongGuess));
+                    System.out.println("YOU WIN!");
+                    System.out.println("The word was: "+word);
+                    break;
+                }
+            }
+
+            else {
+                wrongGuess++;
+                System.out.println("Wrogn guess");
+            }
+        }
+        if (wrongGuess >= 6){
+            System.out.println(getHangmanArt(wrongGuess));
+            System.out.println("GAME OVER");
+            System.out.println("The word was: " + word);
+
+
+        }
 
 
 
